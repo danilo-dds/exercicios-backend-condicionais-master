03e7c353d7de859afca833d8613ecb20f6ffab05
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-04-28 18:28:50 -0300

    exercicio 01 feito

--- 01/index.js
+++ 01/index.js
@@ -1,4 +1,48 @@
-const jogada1 = "pedra"
+const jogada1 = "tesoura"
 const jogada2 = "tesoura"
 
-//seu código aqui
+//vericicanndo primeira jogada com pedra
+
+if (jogada1 === "pedra") {
+
+    if (jogada2 === "tesoura") {
+        console.log(`${jogada1} Ganhou!`);
+
+    } else if (jogada2 === "pedra") {
+        console.log("Deu empate!");
+
+    } else if (jogada2 === "papel") {
+        console.log(`${jogada2} Ganhou!`);
+
+    } else (console.log("Opção invalida"));
+}
+//verificando primeira jogada com tesoura
+
+if (jogada1 === "tesoura") {
+
+    if (jogada2 === "tesoura") {
+        console.log("Deu empate!");
+
+    } else if (jogada2 === "pedra") {
+        console.log(`${jogada2} Ganhou!`);
+
+    } else if (jogada2 === "papel") {
+        console.log(`${jogada1} Ganhou!`);
+
+    } else (console.log("opção invalida"));
+}
+//verificando primeira jogada com papel
+
+if (jogada1 === "papel") {
+
+    if (jogada2 === "tesoura") {
+        console.log(`${jogada2} Ganhou!`);
+
+    } else if (jogada2 === "pedra") {
+        console.log(`${jogada1} Ganhou!`);
+
+    } else if (jogada2 === "papel") {
+        console.log("Deu empate!");
+
+    } else (console.log("opção invalida"));
+}
