6248dff597a668a8d82b7a8d8aedf6b88cf239c5
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-04-29 08:57:51 -0300

    exercicio 06 feito

--- 06/index.js
+++ 06/index.js
@@ -1,3 +1,15 @@
-const alturaEmCm = 185;
+const alturaEmCm = 198;
 
-//seu código aqui
+if (alturaEmCm >= 180) {
+    if (alturaEmCm >= 180 && alturaEmCm <= 185) {
+        console.log("Líbero");
+    } else if (alturaEmCm >= 186 && alturaEmCm <= 195) {
+        console.log("Ponteiro");
+    } else if (alturaEmCm >= 196 && alturaEmCm <= 205) {
+        console.log("Oposto");
+    } else if (alturaEmCm >= 205) {
+        console.log("Central");
+    }
+} else {
+    console.log("Reprovado");
+}
