b64dade41d5527681300635f1dee42396cf419ff
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-04-29 17:57:57 -0300

    exercicio 07 feito

--- 07/index.js
+++ 07/index.js
@@ -1,5 +1,12 @@
 const aposentada = false;
 const portadoraDeDoenca = false;
-const totalDeRendimentos = 3000000; //emCentavos
+const totalDeRendimentos = 2855971; //emCentavos
 
-//seu código aqui
+if (portadoraDeDoenca === true || aposentada === true) {
+    console.log("ISENTA");
+} else if (totalDeRendimentos <= 2855970) {
+    console.log("VAZA LEÃO! JA TA DICICIL SEM VOCÊ");
+
+} else if (totalDeRendimentos > 2855970) {
+    console.log("PEGA LEÃO");
+}
