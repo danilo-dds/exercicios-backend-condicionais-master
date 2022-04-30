50ff58c3779ab921f980c11615be6da4bd3b1d4e
danilo Nascimento <danilodds.nascimento@gmail.com>
2022-04-30 12:45:26 -0300

    exercicio 08 feito

--- 08/index.js
+++ 08/index.js
@@ -1,5 +1,12 @@
-const idade = 18;
+const idade = 15;
 const possuiPatologia = false;
-const altura = 180;
+const altura = 170;
 const ehEstudante = false;
 
+if (idade < 12 || idade > 65 || altura < 150 || possuiPatologia === true) {
+    console.log("ACESSO NEGADO");
+} else if (ehEstudante === true || idade < 18) {
+    console.log("10 Reais");
+} else {
+    console.log("20 Reais");
+}
