5e790b8b6892263a4c395f0ba94d274dd8e60088
danilo Nascimento <danilodds.nascimento@gmail.com>
2022-04-30 13:23:01 -0300

    exercicio 09 feito

--- 09/index.js
+++ 09/index.js
@@ -1,1 +1,15 @@
-const nota = 8.5;
+const nota = 8.6;
+
+if (nota >= 9 && nota <= 10) {
+    console.log("O estudando obteve conceito A");
+} else if (nota >= 8 && nota <= 8.9) {
+    console.log("O estudando obteve conceito B");
+} else if (nota >= 6 && nota <= 7.9) {
+    console.log("O estudando obteve conceito C");
+} else if (nota >= 4 && nota <= 5.9) {
+    console.log("O estudando obteve conceito D");
+} else if (nota < 4 && nota >= 0) {
+    console.log("O estudando obteve conceito E");
+} else {
+    console.log("Nota invalida insira uma nota entre 0 e 10");
+}
