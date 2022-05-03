1907307daa93fbcc7a526ccb5d77036574ee6536
danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-03 06:28:51 -0300

    exercicio 17 feito

--- 17/index.js
+++ 17/index.js
@@ -6,3 +6,8 @@ const quantidadeDoParcelamento = 10;
 
 //valor pago
 const valorPago = 300;
+
+const valorParcelaEmReais = (valorDoProduto / quantidadeDoParcelamento) / 100;
+const parcelasPagas = valorPago / valorParcelaEmReais;
+
+console.log(`Restam ${quantidadeDoParcelamento - parcelasPagas} parcelas de R$ ${valorParcelaEmReais.toFixed(2)} Reais `);
